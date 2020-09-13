30c407b30e98b8a49f306aa7d032cf9501a483d0
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-09-13 19:20:44 +0430

    set for khafonli

--- phoenix/settings_khafonli.py
+++ phoenix/settings_khafonli.py
@@ -7,35 +7,42 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 DEBUG =False
 
 
-ALLOWED_HOSTS = ['www.khafonline.com','khafonline.com','www.hamescctv.com','hamescctv.com','www.imenyar.com','imenyar.com','www.tonusdoor.com','tonusdoor.com']
+ALLOWED_HOSTS = ['www.khafonline.com','khafonline.com']
 
-MYSQL=True
+MYSQL=False
+
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'OPTIONS': {
+#                 'read_default_file': os.path.join(os.path.join(BASE_DIR, 'phoenix'),'secret_khafonli_my_sql.cnf'),
+
+#             },
+#         }
+#     }
 
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'OPTIONS': {
-                'read_default_file': os.path.join(os.path.join(BASE_DIR, 'phoenix'),'secret_khafonli_my_sql.cnf'),
-
-            },
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_local.sqlite3'),
     }
-
+}
 
 TIME_ZONE = 'Asia/Tehran'
 
-SITE_URL='/phoenix/'
+SITE_URL='/phoenix_v1/'
 
 
 ADMIN_URL=SITE_URL+'admin/'
 STATIC_URL = SITE_URL+'static/'
 
-STATIC_ROOT = '/home/khafonli/public_html/phoenix/staticfiles/'
+STATIC_ROOT = '/home/khafonli/public_html/phoenix_v1/staticfiles/'
 
 MEDIA_URL =  SITE_URL+'media/'
-MEDIA_ROOT = '/home/khafonli/public_html/phoenix/media/'
-STATICFILES_DIRS=['/home/khafonli/phoenix/static/']
+MEDIA_ROOT = '/home/khafonli/public_html/phoenix_v1/media/'
+STATICFILES_DIRS=['/home/khafonli/phoenix_v1/static/']
 PUSHER_IS_ENABLE=False
 REMOTE_MEDIA=False
 COMING_SOON=False
