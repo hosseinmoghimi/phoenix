9209c8e1604fc1254923f8de70ac712894c586c4
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-09-28 14:33:35 +0330

    ...

--- tutorial/views.py
+++ tutorial/views.py
@@ -4,6 +4,8 @@ from .apps import APP_NAME
 from app.views import getContext as AppContext
 from django.views import View
 from .repo import LessonRepo
+from app.repo import TagRepo,LikeRepo
+from app.forms import AddLikeForm,AddCommentForm,DeleteCommentForm
 
 TEMPLATE_ROOT='tutorial/'
 def getContext(request):
@@ -19,7 +21,20 @@ class BasicView(View):
         return render(request,TEMPLATE_ROOT+'index.html',context)
     def lesson(self,request,lesson_id,*args, **kwargs):
         context=getContext(request)
-        context['page']=LessonRepo(user=request.user).lesson(lesson_id)
+        page=LessonRepo(user=request.user).lesson(lesson_id)
+        context['page']=page
+        page_id=lesson_id
+        if page is None:
+            raise Http404
+        tags=TagRepo(user=request.user).list_top()
+        context['tags']=tags
+        context['add_like_form']=AddLikeForm()
+        context['get_edit_url']=page.get_edit_url()
+        context['add_comment_form']=AddCommentForm()
+        context['delete_comment_form']=DeleteCommentForm()
+        comments_s=json.dumps(CommentSerializer(page.comments,many=True).data)
+        context['comments_s']=comments_s
+        context['my_like']=LikeRepo(user=request.user,object_type='Page').my_like(object_id=page_id)
         
         return render(request,'avo/page.html',context)
 
