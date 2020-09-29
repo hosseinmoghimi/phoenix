601730295fd20d64504a7c742251c2753a2c42d2
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-09-29 17:40:14 +0330

    ...

--- phoenix/settings_khafonli.py
+++ phoenix/settings_khafonli.py
@@ -1,4 +1,4 @@
-#source /home/khafonli/virtualenv/phoenix/3.7/bin/activate && cd /home/khafonli/phoenix && git pull && python manage.py migrate && python manage.py collectstatic --no-input
+#source /home2/khafonli/virtualenv/phoenix/3.7/bin/activate && cd /home2/khafonli/phoenix && git pull && python manage.py migrate && python manage.py collectstatic --no-input
 from pathlib import Path
 import os
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -40,11 +40,11 @@ SITE_URL='/'
 ADMIN_URL=SITE_URL+'admin/'
 STATIC_URL = SITE_URL+'static/'
 
-STATIC_ROOT = '/home/khafonli/public_html/phoenix/staticfiles/'
+STATIC_ROOT = '/home2/khafonli/public_html/phoenix/staticfiles/'
 
 MEDIA_URL =  SITE_URL+'media/'
-MEDIA_ROOT = '/home/khafonli/public_html/phoenix/media/'
-STATICFILES_DIRS=['/home/khafonli/phoenix/static/']
+MEDIA_ROOT = '/home2/khafonli/public_html/phoenix/media/'
+STATICFILES_DIRS=['/home2/khafonli/phoenix/static/']
 PUSHER_IS_ENABLE=True
 REMOTE_MEDIA=False
 COMING_SOON=False
