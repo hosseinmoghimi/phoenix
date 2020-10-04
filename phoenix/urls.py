c14113358257097b78385a72b3bb702efbea2aec
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-10-04 23:46:03 +0330

    ...

--- phoenix/urls.py
+++ phoenix/urls.py
@@ -10,7 +10,7 @@ from .settings import SERVER_ON_HEROKU
 from app.feeder import LatestEntriesFeed,SiteMapFeeder
 
 urlpatterns = [
-    path('', include('projectmanager.urls')),
+    path('', include('app.urls')),
     path('phoenix_v1/', include('app.urls')),
     
     path('pusher/', include('leopusher.urls')),
