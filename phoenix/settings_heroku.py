b084cd8fdc83aad7d616a40f219dec54cc68b416
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2020-10-06 00:16:46 +0330

    ...

--- phoenix/settings_heroku.py
+++ phoenix/settings_heroku.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY="cz5^v3qa%kxwwlidp&&l)vy3@z78j#^u7mm+c4b)+@yjs42k6x"
 
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['herokuapp.com']
 
